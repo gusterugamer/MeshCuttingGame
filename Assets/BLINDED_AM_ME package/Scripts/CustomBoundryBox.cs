7c6d5eb50176817569730cf9e39b833b8614d2e0
Palade Silviu-Marius <marius@blastproofgames.com>
2020-08-04 17:55:13 +0300

    MehCutter

---
+++ Assets/BLINDED_AM_ME package/Scripts/CustomBoundryBox.cs
@@ -0,0 +1,95 @@
+﻿using JetBrains.Annotations;
+using Microsoft.Win32.SafeHandles;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomBoundryBox : MonoBehaviour
+{
+    public struct BoundaryPoint
+    {
+        public Vector3 m_pos;
+
+        public BoundaryPoint(Vector3 pos)
+        {
+            m_pos = pos;
+        }
+        public BoundaryPoint(float x, float y)
+        {
+            m_pos = new Vector3(x, y, 0.0f);
+        }
+
+        public static BoundaryPoint zero => new BoundaryPoint(0.0f,0.0f);       
+    }
+
+    [SerializeField] private GameObject m_toCutObject;
+    public BoundaryPoint[] m_CustomBox;
+    public BoundaryPoint[] m_newBoundaryPoints; 
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        CreateCustomBoundary();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        DrawCustomBoundary();
+    }
+
+    void CreateCustomBoundary()
+    {
+        Vector3 center = m_toCutObject.GetComponent<MeshFilter>().mesh.bounds.center;
+        Vector3 minPrime = m_toCutObject.GetComponent<MeshFilter>().mesh.bounds.min;
+        Vector3 maxPrime = m_toCutObject.GetComponent<MeshFilter>().mesh.bounds.max;
+        Vector3 min = center + minPrime;
+        Vector3 max = center + maxPrime;
+
+        m_CustomBox = new BoundaryPoint[4];
+
+        m_CustomBox[0] = new BoundaryPoint(min);       
+        m_CustomBox[1] = new BoundaryPoint(new Vector3(max.x, min.y, min.z));
+        m_CustomBox[2] = new BoundaryPoint(new Vector3(max.x, max.y, min.z));
+        m_CustomBox[3] = new BoundaryPoint(new Vector3(min.x, max.y, min.z));
+    }
+
+    void DrawCustomBoundary()
+    {
+        int length = m_CustomBox.Length;
+        for (int i= 0; i<length;i++)
+        {
+            if (i + 3 < length)
+            {
+                Debug.DrawLine(transform.position + m_CustomBox[i].m_pos, transform.position + m_CustomBox[(i + 3)].m_pos, Color.red);
+            }
+            if (i + 1 < length)
+            {
+                Debug.DrawLine(transform.position + m_CustomBox[i].m_pos, transform.position + m_CustomBox[(i + 1)].m_pos, Color.red);
+            }
+            if (i + 4 < length)
+            {
+                Debug.DrawLine(transform.position + m_CustomBox[i].m_pos, transform.position + m_CustomBox[(i + 4)].m_pos, Color.red);
+            }
+        }
+    }
+
+    public List<BoundaryPoint> GetIntersections(Vector3 startPoint, Vector3 endPoint, Transform victim)
+    {
+        BoundaryPoint tempIntersectionPoint = BoundaryPoint.zero;
+        List<BoundaryPoint> pointsList = new List<BoundaryPoint>();
+        int length = m_CustomBox.Length;
+        for (int i=0;i<m_CustomBox.Length;i++)
+        {
+            //tempIntersectionPoint = Math.getLineLineIntersection(startPoint, endPoint,
+            //                                                     m_CustomBox[i], m_CustomBox[(i + 1) % length], victim);
+            Vector2 inters;
+            bool sax = Math.LineSegmentsIntersection(startPoint, endPoint, m_CustomBox[i].m_pos, m_CustomBox[(i + 1) % length].m_pos,out inters);
+            tempIntersectionPoint.m_pos = inters;
+
+            pointsList.Add(tempIntersectionPoint);
+        }       
+        return pointsList;
+    }
+}
---
+++ Assets/BLINDED_AM_ME package/Scripts/ExampleUseof_MeshCut.cs
@@ -0,0 +1,84 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class ExampleUseof_MeshCut : MonoBehaviour {
+
+	public Material capMaterial;
+	public List<CustomBoundryBox.BoundaryPoint> list;
+
+	private Vector3 startPos;
+	private Vector3 endPos;
+	private Camera mainCam;
+
+	// Use this for initialization
+	void Start () {
+		mainCam = Camera.main;		
+	}
+	
+	void Update(){
+		GetMousePosition();
+		Debug.DrawLine(startPos, endPos, Color.black);
+	}
+
+	void CutStuff(Vector3 startPos, Vector3 endPos)
+    {
+		RaycastHit hit;
+
+		if (Physics.Raycast(transform.position, transform.forward, out hit))
+		{
+
+			GameObject victim = hit.collider.gameObject;
+
+			GameObject[] pieces = BLINDED_AM_ME.MeshCut.Cut(victim, transform.position, transform.right, capMaterial, startPos, endPos);
+			list = BLINDED_AM_ME.MeshCut.intersectionPoint;
+
+			foreach (var point in list)
+            {
+				Debug.Log("Point: " + point.m_pos);
+            }
+
+			if (!pieces[1].GetComponent<DrawBounds>())
+				pieces[1].AddComponent<DrawBounds>();
+
+			//Destroy(pieces[1], 1);
+		}
+	}
+
+	void GetMousePosition()
+    {
+		if (Input.GetMouseButtonDown(0))
+		{
+			startPos = Input.mousePosition;
+			startPos.z = 10.0f;
+		}
+		if (Input.GetMouseButtonUp(0))
+		{
+			endPos = Input.mousePosition;
+			endPos.z = 10.0f;
+			mousePosToWorld();
+		}
+	}
+
+	void mousePosToWorld()
+    {
+		startPos = mainCam.ScreenToWorldPoint(startPos);
+		endPos = mainCam.ScreenToWorldPoint(endPos);
+		CutStuff(startPos, endPos);
+		Debug.Log("MousePos: " + "start: " + startPos + "end: " + endPos);
+    }
+
+	void OnDrawGizmosSelected() {
+
+		Gizmos.color = Color.green;
+
+		Gizmos.DrawLine(transform.position, transform.position + transform.forward * 5.0f);
+		Gizmos.DrawLine(transform.position + transform.up * 0.5f, transform.position + transform.up * 0.5f + transform.forward * 5.0f);
+		Gizmos.DrawLine(transform.position + -transform.up * 0.5f, transform.position + -transform.up * 0.5f + transform.forward * 5.0f);
+
+		Gizmos.DrawLine(transform.position, transform.position + transform.up * 0.5f);
+		Gizmos.DrawLine(transform.position,  transform.position + -transform.up * 0.5f);
+
+	}
+
+}
