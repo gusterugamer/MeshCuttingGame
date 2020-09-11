309cb84a8b8f91b0e5c9f8ba90d3e09580fbcf01
Palade Silviu-Marius <marius@blastproofgames.com>
2020-09-11 17:43:14 +0300

    Added score and level manager

---
+++ Assets/Scripts/LevelManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager : MonoBehaviour
+{
+    [SerializeField] CustomBoundryBox cbm;
+
+    private static Score score;
+   
+    void Start()
+    {
+        score = new Score(cbm.Area);
+    }    
+}
---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score
+{
+    float _startArea;
+    float _currentArea;
+
+    public Score(float startArea)
+    {
+        _startArea = startArea;
+    }
+
+    public float StartArea { get => _startArea; }
+    public float CurrentArea { get => _currentArea; }
+
+    public int progressPercent()
+    {
+        return (int)((CurrentArea / StartArea) * 100f);
+    }
+
+    public void UpdateCurrentScore(float currentArea)
+    {
+        _currentArea = currentArea;
+    }
+}
